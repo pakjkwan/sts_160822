5625a35dff67ac6a87c887091f2165fe76883412
hanbitweekend <pakjkwan2@gmail.com>
2016-08-25 17:27:57 +0900

    day end

--- src/main/webapp/resources/js/application.js
+++ src/main/webapp/resources/js/application.js
@@ -1,118 +1,66 @@
 var app = (function(){
 	var init = function(context) {
-		sessionStorage.setItem('context',context);
-		sessionStorage.setItem('js',context+'/resources/js');
-		sessionStorage.setItem('css',context+'/resources/css');
-		sessionStorage.setItem('img',context+'/resources/img');
-		move();
+		onCreate();
+		session.init(context);
 		member.init();
+		user.init();
+		account.init();
+		kaup.init();
+		grade.init();
+	};
+	var context = function(){return session.getContextPath();};
+	var js = function(){return session.getJavascriptPath('js');};
+	var css = function(){return session.getCssPath('css');};
+	var img = function(){return session.getImagePath('img');};
+	var setContentView = function(){
 		$('footer').addClass('bottom');
 		$('#global_content').addClass('box');
 		$('#global_content a').addClass('cursor');
-		$('#global_content_a_regist').text('SIGN UP').click(function(){util.move('member','regist');});
-		$('#global_content_a_login').text('LOG IN').click(function(){util.move('member','login');});
+		$('#global_content_a_regist').text('SIGN UP').click(function(){controller.move('member','regist');});
+		$('#global_content_a_login').text('LOG IN').click(function(){controller.move('member','login');});
 		$('#global_content_a_admin').text('ADMIN MODE').click(function(){admin.check();});
 	};
-	var context = function(){
-		return sessionStorage.getItem('context');
-	};
-	var js = function(){
-		return sessionStorage.getItem('js');
-	};
-	var css = function(){
-		return sessionStorage.getItem('css');
-	};
-	var img = function(){
-		return sessionStorage.getItem('img');
-	};
-	var move = function(){
-		$('#title').click(function(){util.home();});
-		$('#a_member').click(function(){util.move('member','main');});
-		$('#a_grade').click(function(){util.move('grade','main');});
-		$('#a_account').click(function(){util.move('account','main');});
-		$('#a_shool').click(function(){util.move('global','school_info');});
+	var onCreate = function(){
+		setContentView();
+		$('#title').click(function(){controller.home();});
+		$('#a_member').click(function(){controller.move('member','main');});
+		$('#a_grade').click(function(){controller.move('grade','main');});
+		$('#a_account').click(function(){controller.move('account','main');});
+		$('#a_shool').click(function(){controller.move('global','school_info');});
 	};
 	
 	return {
 		init : init,
-		move : move,
+		onCreate : onCreate,
+		setContentView : setContentView,
 		context : context,
 		img : img,
 		js : js,
 		css : css
 	}
 })();
-
-var admin = (function() {
-	var _pass;
-    var	getPass = function(){return this._pass;};
-    var setPass = function(pass){this._pass=pass;};
-    return {
-    	getPass : getPass,
-    	setPass : setPass,
-    	check : function() {
-    		setPass(1);
-			var isAdmin = confirm('관리자입니까?');
-			if (!isAdmin) {
-				alert('관리자만 접근 가능합니다.');
-			} else {
-				var password = prompt('관리자 비번을 입력바랍니다');
-				if(password == getPass()){
-					location.href = app.context()+'/admin/main';
-				}else{
-					alert('관리자 비번이 틀립니다.');
-				}
-			}
-		}
-    };
-})();
-var util = (function(){
-	var _page,_directory;
-	var setPage=function(page){this._page=page;};
-	var setDirectory=function(directory){this._directory=directory;};
-	var getPage = function(){return this._page;};
-	var getDirectory = function(){return this._directory;};
-	return {
-		setPage : setPage,
-		getPage : getPage,
-		setDirectory : setDirectory,
-		getDirectory : getDirectory,
-		move : function(directory,page){
-			setDirectory(directory);
-			setPage(page);
-			location.href = app.context()+'/'+getDirectory()+'/'+getPage();
-				
-		},
-		isNumber : function(value){
-			return typeof value === 'number' && isFinite(value);
-		},
-		home : function(){location.href=app.context()+'/'}
-		
-	};
-})();
-
-
 var user = (function(){
-	var context = sessionStorage.getItem("context");
+	var init = function(){onCreate();};
+	var setContentView = function(){
+		$('#member_content_img_home').attr('src',app.img()+'/home.png');
+	};
+	var onCreate = function(){
+		setContentView();
+		$('#bt_bom').click(function(){controller.move('','bom');});
+		$('#bt_dom').click(function(){controller.move('','dom');});
+		$('#bt_kaup').click(function(){controller.move('','kaup');});
+		$('#bt_account').click(function(){controller.move('','account');});
+		$('#a_regist').click(function(){controller.move('account','regist');});
+		$('#a_withdraw').click(function(){controller.move('account','withdraw');});
+		$('#a_deposit').click(function(){controller.move('account','deposit');});
+		$('#a_list').click(function(){controller.move('account','list');});
+		$('#a_update').click(function(){controller.move('account','update');});
+		$('#a_count').click(function(){controller.move('account','count');});
+		$('#a_search').click(function(){controller.move('account','search');});
+		$('#a_delete').click(function(){controller.move('account','delete');});
+	};
 	return {
-		init : function() {
-			$('#bt_bom').click(function(){move(context,'bom');});
-			$('#bt_dom').click(function(){move(context,'dom');});
-			$('#bt_kaup').click(function(){move(context,'kaup');});
-			$('#bt_account').click(function(){move(context,'account');});
-		},
-		account : function(){
-			$('#a_regist').click(function(){location.href=app.context()+"/account/regist"});
-			$('#a_withdraw').click(function(){location.href=app.context()+"/account/withdraw"});
-			$('#a_deposit').click(function(){location.href=app.context()+"/account/deposit"});
-			$('#a_list').click(function(){location.href=app.context()+"/account/list"});
-			$('#a_update').click(function(){location.href=app.context()+"/account/update"});
-			$('#a_count').click(function(){location.href=app.context()+"/account/count"});
-			$('#a_search').click(function(){location.href=app.context()+"/account/search"});
-			$('#a_delete').click(function(){location.href=app.context()+"/account/delete"});
-			
-			$('#member_content_img_home').attr('src',app.img()+'/home.png');
-		}
+		init : init
 	};
 })();
 var account = (function(){
@@ -121,17 +69,21 @@ var account = (function(){
 	var getAccountNo = function(){return this._account_no;};
 	var setMoney = function(money){this._money=money;};
 	var getMoney = function(){return this._money;};
+	var init = function(){onCreate();};
+	var setContentView = function(){};
+	var onCreate = function(){
+		setContentView();
+		$('#bt_spec_show').click(member.spec());
+		$('#bt_make_account').click(this.spec());
+		$('#bt_deposit').click(this.deposit());
+		$('#bt_withdraw').click(this.withdraw());
+	};
 	return {
 		setAccountNo : setAccountNo,
 		getAccountNo : getAccountNo,
 		setMoney : setMoney,
 		getMoney : getMoney,
-		init : function() {
-			$('#bt_spec_show').click(member.spec());
-			$('#bt_make_account').click(this.spec());
-			$('#bt_deposit').click(this.deposit());
-			$('#bt_withdraw').click(this.withdraw());
-		},
+		init : init,
 		spec : function(){
 			setAccountNo(Math.floor(Math.random()*899999)+100000);
 			setMoney(0);
@@ -139,7 +91,7 @@ var account = (function(){
 		},
 		deposit : function (){
 			var r_acc = document.querySelector('#result_account').innerText;
-			console.log('계좌번호 : '+r_acc);
+			console.log('ACCOUNT NO : '+r_acc);
 			switch(typeof r_acc){
 				case 'number' : console.log('this is number type');break;
 				case 'string' : console.log('this is string type');break;
@@ -152,10 +104,10 @@ var account = (function(){
 			}else{
 				var input_money = Number(document.querySelector('#money').value);
 				var rest_money = getMoney();
-				console.log('인풋 머니 타입 체크 : '+(typeof input_money === 'number'));
-				console.log('잔액 타입 체크 : '+(typeof rest_money === 'number'));
+				console.log('TYPE CHECK OF INPUT MONEY : '+(typeof input_money === 'number'));
+				console.log('TYPE CHECK OF REST MONEY : '+(typeof rest_money === 'number'));
 				setMoney(input_money+rest_money);
-				console.log('입금액 : '+getMoney());
+				console.log('INPUT MONEY : '+getMoney());
 				document.querySelector('#rest_money').innerHTML=getMoney();
 			}
 		},
@@ -175,7 +127,48 @@ var member = (function(){
 	var getSSN = function(){return this._ssn;}
 	var getName = function(){return this._name;}
 	var getGender = function(){return this._gender;}
-	
+	var init = function(){onCreate()};
+	var setContentView = function(){
+		$('#member_content_img_home').attr('src',app.img()+'/home.png').css('width','30px');
+		$('#member_content_a_home').attr('alt','home').click(function(){controller.home();});
+		$('#member_content').addClass('box').css('font-size','20px');
+		$('#member_content > article').css('width','300px').addClass('center').addClass('text_left');
+		$('#member_content a').css('font-size','15px').addClass('cursor');
+		$('#member_content > h1').text('MEMBER MGMT');
+		$('#member_content_ol > li > a').addClass('remove_underline');
+		$('#member_content_ol > li:first > a').text('SIGN UP');
+		$('#member_content_ol > li:nth(1) > a').text('DETAIL');
+		$('#member_content_ol > li:nth(2) > a').text('UPDATE');
+		$('#member_content_ol > li:nth(3) > a').text('DELETE');
+		$('#member_content_ol > li:nth(4) > a').text('LOG IN');
+		$('#member_content_ol > li:nth(5) > a').text('LOG OUT');
+		$('#member_content_ol > li:nth(6) > a').text('LIST');
+		$('#member_content_ol > li:nth(7) > a').text('SEARCH');
+		$('#member_content_ol > li:nth(8) > a').text('COUNT');
+		$('#member_regist').addClass('box');
+		$('#member_regist #bt_join').addClass('btn').addClass(' btn-primary');
+		$('#member_regist #bt_cancel').addClass('btn').addClass(' btn-danger');
+		$('#member_regist_form').addClass('form-horizontal');
+		$('#member_regist_form > div').addClass('form-group').addClass('form-group-lg');
+		$('#member_regist_form > div > label').addClass('col-sm-2').addClass('control-label');
+		$('#member_regist_form > div > div').addClass('col-sm-10');
+		$('#member_regist_form > div > div > input').addClass('form-control');
+		$('#member_regist #rd_major > label:gt(1)').addClass('radio-inline');
+		$('#member_regist #ck_subject').addClass('checkbox');
+		$('#member_regist #ck_subject > label').addClass('checkbox-inline');
+	};
+	var onCreate = function(){
+		setContentView();
+		$('#regist').click(function(){controller.move('member','regist');});
+		$('#detail').click(function(){controller.move('member','detail');});
+		$('#update').click(function(){controller.move('member','update');});
+		$('#delete').click(function(){controller.move('member','delete');});
+		$('#login').click(function(){controller.move('member','login');});
+		$('#logout').click(function(){controller.move('member','logout');});
+		$('#list').click(function(){controller.move('member','list');});
+		$('#find_by').click(function(){controller.move('member','find_by');});
+		$('#count').click(function(){controller.move('member','count');});
+	};
 	return {
 		setSSN : setSSN,
 		setName : setName,
@@ -185,6 +178,7 @@ var member = (function(){
 		getAge : getAge,
 		getSSN : getSSN,
 		getGender : getGender,
+		init : init,
 		spec : function (){
 			console.log('SET SSN'+document.querySelector('#ssn').value);
 			setSSN(document.querySelector('#ssn').value);
@@ -225,14 +219,16 @@ var member = (function(){
 		
 	};	
 })();
-
 var kaup = (function(){
+	var init = function(){onCreate();};
+	var setContentView = function(){};
+	var onCreate = function(){
+		setContentView();
+		document.getElementById('bt_kaup_calc').addEventListener('click',this.calc,false);
+	};
 	return {
-		init : function (){
-			document.getElementById('bt_kaup_calc').addEventListener('click',this.calc,false);
-		},
+		init : init,
 		calc : function (){
-			alert('카우푸 칼크 클릭');
 			var name=document.querySelector('#name').value;
 			var height=document.querySelector('#height').value;
 			var weight=document.querySelector('#weight').value;
@@ -256,97 +252,106 @@ var kaup = (function(){
 			}
 			document.getElementById('result').innerHTML=name+'의 카우푸결과'+result;
 		}
-		
 	};
 })();
 var grade = (function(){
+	var init = function(){onCreate();};
+	var setContentView = function(){
+		$('#member_content_img_home').attr('src',app.img()+'/home.png');
+		$('#grade_content').addClass('box');
+		$('#img_home').css('width','30px');
+		$('#grade_content > article').css('width','300px').css('text-align','left').css('margin','0 auto');
+		$('#title').css('font-size','40px');
+	};
+	var onCreate = function(){
+		setContentView();
+		$('#g_regist').click(function(){controller.move('grade','regist');});
+		$('#g_update').click(function(){controller.move('grade','update');});
+		$('#g_delete').click(function(){controller.move('grade','delete');});
+		$('#g_list').click(function(){controller.move('grade','list');});
+		$('#g_count').click(function(){controller.move('grade','count');});
+		$('#g_find').click(function(){controller.move('grade','find');});
+		$('#a_regist').click(function() {location.href = "${context}/grade/regist.do";});
+		$('#a_update').click(function() {location.href ="${context}/grade/update.do";});
+		$('#a_delete').click(function() {location.href = "${context}/grade/delete.do";});
+		$('#a_list').click(function() {location.href = "${context}/grade/list.do";});
+		$('#a_count').click(function() {location.href = "${context}/grade/count.do";});
+		$('#a_find').click(function() {location.href = "${context}/grade/search.do";});
+	};
 	return {
-		init : function(){
-			$('#g_regist').click(function(){
-				location.href=app.context()+"/grade/regist"});
-			$('#g_update').click(function(){location.href=app.context()+"/grade/update"});
-			$('#g_delete').click(function(){location.href=app.context()+"/grade/delete"});
-			$('#g_list').click(function(){location.href=app.context()+"/grade/list"});
-			$('#g_count').click(function(){location.href=app.context()+"/grade/count"});
-			$('#g_find').click(function(){location.href=app.context()+"/grade/find"});
-			
-			$('#member_content_img_home').attr('src',app.img()+'/home.png');
+		init : init,
+	};
+})();
+
+var admin = (function() {
+	var _pass;
+    var	getPass = function(){return this._pass;};
+    var setPass = function(pass){this._pass=pass;};
+    return {
+    	getPass : getPass,
+    	setPass : setPass,
+    	check : function() {
+    		setPass(1);
+			var isAdmin = confirm('관리자입니까?');
+			if (!isAdmin) {
+				alert('관리자만 접근 가능합니다.');
+			} else {
+				var password = prompt('관리자 비번을 입력바랍니다');
+				if(password == getPass()){
+					controller.move('admin','main');
+				}else{
+					alert('관리자 비번이 틀립니다.');
+				}
+			}
+		}
+    };
+})();
+var session = (function(){
+	var init = function(context){
+		sessionStorage.setItem('context',context);
+		sessionStorage.setItem('js',context+'/resources/js');
+		sessionStorage.setItem('css',context+'/resources/css');
+		sessionStorage.setItem('img',context+'/resources/img');
+	};
+	var getContextPath = function(){return sessionStorage.getItem('context');};
+	var getJavascriptPath = function(){return sessionStorage.getItem('js');};
+	var getCssPath = function(){return sessionStorage.getItem('js');};
+	var getImagePath = function(){return sessionStorage.getItem('img');};
+	return {
+		init : init,
+		getContextPath : getContextPath,
+		getJavascriptPath : getJavascriptPath,
+		getCssPath : getCssPath,
+		getImagePath : getImagePath
+	};
+	
+})();
+var controller = (function(){
+	var _page,_directory;
+	var setPage=function(page){this._page=page;};
+	var setDirectory=function(directory){this._directory=directory;};
+	var getPage = function(){return this._page;};
+	var getDirectory = function(){return this._directory;};
+	return {
+		setPage : setPage,
+		getPage : getPage,
+		setDirectory : setDirectory,
+		getDirectory : getDirectory,
+		move : function(directory,page){
+			setDirectory(directory);
+			setPage(page);
+			location.href = app.context()+'/'+getDirectory()+'/'+getPage();
 		},
-		init2 : function(){
-			$('#grade_content').addClass('box');
-			$('#img_home').css('width','30px');
-			$('#grade_content > article')
-			   .css('width','300px')
-			   .css('text-align','left')
-			   .css('margin','0 auto');
-			$('#title').css('font-size','40px');
-			$('#a_regist').click(function() {
-				location.href = "${context}/grade/regist.do";
-			});
-			$('#a_update').click(function() {
-				location.href ="${context}/grade/update.do";
-			});
-			$('#a_delete').click(function() {
-				location.href = "${context}/grade/delete.do";
-			});
-			$('#a_list').click(function() {
-				location.href = "${context}/grade/list.do";
-			});
-			$('#a_count').click(function() {
-				location.href = "${context}/grade/count.do";
-			});
-			$('#a_find').click(function() {
-				location.href = "${context}/grade/search.do";
-			});
-		}
+		home : function(){location.href=app.context()+'/'}
 	};
 })();
-var member = (function(){
+var util = (function(){
 	return {
-		init : function(){
-			$('#regist').click(function(){location.href=app.context()+"/member/regist"});
-			$('#detail').click(function(){location.href=app.context()+"/member/detail"});
-			$('#update').click(function(){location.href=app.context()+"/member/update"});
-			$('#delete').click(function(){location.href=app.context()+"/member/delete"});
-			$('#login').click(function(){location.href=app.context()+"/member/login"});
-			$('#logout').click(function(){location.href=app.context()+"/member/logout"});
-			$('#list').click(function(){location.href=app.context()+"/member/list"});
-			$('#find_by').click(function(){location.href=app.context()+"/member/find_by"});
-			$('#count').click(function(){location.href=app.context()+"/member/count"});
-			$('#member_content_img_home').attr('src',app.img()+'/home.png').css('width','30px');
-			$('#member_content_a_home').attr('alt','home').click(function(){location.href=app.context()+"/"});
-			$('#member_content').addClass('box').css('font-size','20px');
-			$('#member_content > article').css('width','300px').addClass('center').addClass('text_left');
-			$('#member_content a').css('font-size','15px').addClass('cursor');
-			$('#member_content > h1').text('MEMBER MGMT');
-			$('#member_content_ol > li > a').addClass('remove_underline');
-			$('#member_content_ol > li:first > a').text('SIGN UP');
-			$('#member_content_ol > li:nth(1) > a').text('DETAIL');
-			$('#member_content_ol > li:nth(2) > a').text('UPDATE');
-			$('#member_content_ol > li:nth(3) > a').text('DELETE');
-			$('#member_content_ol > li:nth(4) > a').text('LOG IN');
-			$('#member_content_ol > li:nth(5) > a').text('LOG OUT');
-			$('#member_content_ol > li:nth(6) > a').text('LIST');
-			$('#member_content_ol > li:nth(7) > a').text('SEARCH');
-			$('#member_content_ol > li:nth(8) > a').text('COUNT');
-			$('#member_regist').addClass('box');
-			$('#member_regist #bt_join').addClass('btn').addClass(' btn-primary');
-			$('#member_regist #bt_cancel').addClass('btn').addClass(' btn-danger');
-			$('#member_regist_form').addClass('form-horizontal');
-			$('#member_regist_form > div').addClass('form-group').addClass('form-group-lg');
-			$('#member_regist_form > div > label').addClass('col-sm-2').addClass('control-label');
-			$('#member_regist_form > div > div').addClass('col-sm-10');
-			$('#member_regist_form > div > div > input').addClass('form-control');
-			$('#member_regist #rd_major > label:gt(1)').addClass('radio-inline');
-			$('#member_regist #ck_subject').addClass('checkbox');
-			$('#member_regist #ck_subject > label').addClass('checkbox-inline');
-
-
-
+		isNumber : function(value){
+			return typeof value === 'number' && isFinite(value);
 		}
 	};
 })();
-
 
 
 
