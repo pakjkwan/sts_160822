5a447405050109f116d40bb430a1397b19008257
hanbitweekend <pakjkwan2@gmail.com>
2016-09-01 10:41:43 +0900

    10:41

--- src/main/java/com/hanbit/web/home/HomeController.java
+++ src/main/java/com/hanbit/web/home/HomeController.java
@@ -11,29 +11,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 * git add --all
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome PUBLIC! The client locale is {}.", locale);
-		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
 		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+		logger.info("SERVER START TIME IS {}", formattedDate);
 		return "public:public/content.tiles";
 	}
 	@RequestMapping("/global/school_info")
--- src/main/java/com/hanbit/web/member/MemberController.java
+++ src/main/java/com/hanbit/web/member/MemberController.java
@@ -6,13 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.hanbit.web.subject.SubjectMemberVO;
 
 @Controller
-@SessionAttributes("user")
+@SessionAttributes({"user","context","js","css","img"})
 @RequestMapping("/member")
 public class MemberController {
 	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
@@ -23,32 +24,30 @@ public class MemberController {
 			@RequestParam("search_option")String option,
 			@RequestParam("context")String context,
 			Model model){
-		logger.info("MemberController ! findById : {}","??");
-		System.out.println("검색어:"+keyword);
-		System.out.println("옵션 :"+option);
-		System.out.println("context :"+context);
+		logger.info("TO SEARCH KEYWORD IS {}",keyword);
+		logger.info("TO SEARCH OPTION IS {}",option);
+		logger.info("CONTEXT IS {}",context);
 		MemberVO member = (MemberVO) service.findById(keyword);
-		System.out.println("NAME : "+member.getName());
-		System.out.println("이미지:"+member.getProfileImg());
+		logger.info("NAME : "+member.getName());
+		logger.info("이미지:"+member.getProfileImg());
 		model.addAttribute("member", member);
 		model.addAttribute("img", context+"/resources/img");
 		return "admin:member/detail.tiles";
 	}
-	@RequestMapping("/login/execute")
-	public String executeLogin(@RequestParam("id") String id,
+	@RequestMapping(value="/login",method=RequestMethod.POST)
+	public String login(@RequestParam("id") String id,
 			@RequestParam("pw")String pw,
 			@RequestParam("context")String context,
 			Model model) {
-		logger.info("MemberController ! login() ");
-		System.out.println("TRYING TO LOGIN  ID :"+id);
-		System.out.println("CONTEXT :"+context);
+		logger.info("TO LOGIN ID IS {}",id);
+		logger.info("TO LOGIN PW IS {}",pw);
+		logger.info("CONTEXT IS {}",context);
 		MemberVO member = new MemberVO();
-		System.out.println("======new MemberVO() ====");
 		member.setId(id);
 		member.setPw(pw);
-		System.out.println("======서비스 login 가기 직전 ====");
 		SubjectMemberVO sm = service.login(member);
 		model.addAttribute("user",sm);
+		model.addAttribute("context",context);
 		model.addAttribute("js", context+"/resources/js");
 		model.addAttribute("css", context+"/resources/css");
 		model.addAttribute("img", context+"/resources/img");
@@ -57,52 +56,52 @@ public class MemberController {
 	// --- MOVE ---
 	@RequestMapping("/main")
 	public String moveMain() {
-		logger.info("MemberController ! goMain() ");	
+		logger.info("GO TO {}","main");
 		return "admin:member/content.tiles";
 	}
 	@RequestMapping("/regist")
 	public String moveRegist() {
-		logger.info("MemberController ! regist() ");
+		logger.info("GO TO {}","regist");
 		return "public:member/regist.tiles";
 	}
 	@RequestMapping("/detail")
 	public String moveDetail() {
-		logger.info("MemberController ! detail() ");
+		logger.info("GO TO {}","detail");
 		return "member/detail.tiles";
 	}
 	@RequestMapping("/update")
 	public String moveUpdate() {
-		logger.info("MemberController ! update() ");
+		logger.info("GO TO {}","update");
 		return "member/update.tiles";
 	}
 	@RequestMapping("/delete")
 	public String moveDelete() {
-		logger.info("MemberController ! delete() ");
+		logger.info("GO TO {}","delete");
 		return "member/delete.tiles";
 	}
 	@RequestMapping("/login")
-	public String moveLogin() {
-		logger.info("MemberController ! login() ");
+	public String login() {
+		logger.info("GO TO {}","login");
 		return "public:member/login.tiles";
 	}
 	@RequestMapping("/logout")
 	public String moveLogout() {
-		logger.info("MemberController ! logout() ");
+		logger.info("GO TO {}","logout");
 		return "member/logout.tiles";
 	}
 	@RequestMapping("/list")
 	public String moveList() {
-		logger.info("MemberController ! list() ");
+		logger.info("GO TO {}","list");
 		return "admin:member/list.tiles";
 	}
 	@RequestMapping("/find")
 	public String moveFindBy() {
-		logger.info("MemberController ! find_by() ");
+		logger.info("GO TO {}","find");
 		return "admin:member/find_by.tiles";
 	}
 	@RequestMapping("/count")
 	public String moveCount() {
-		logger.info("MemberController ! count() ");
+		logger.info("GO TO {}","count");
 		return "admin:member/count.tiles";
 	}
 }
